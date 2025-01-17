7dade0f4ba2d7893ae83abd162ef3161ab108b7d
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2025-01-18 00:55:01 +0200

    db_script

---
+++ sql_script.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE 'amazon-el8alaba';
+
+USE `amazon-el8alaba`;
+
+
+CREATE TABLE Product (
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         name VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE ProductDetails (
+                                id INT PRIMARY KEY,
+                                expiration_date DATE NOT NULL,
+                                manufacturer VARCHAR(255) NOT NULL,
+                                price DECIMAL(10, 2) NOT NULL,
+                                available BOOLEAN NOT NULL,
+                                image LONGBLOB,
+                                CONSTRAINT fk_product FOREIGN KEY (id) REFERENCES Product(id)
+);
