37bd44b324810640b82f5c511293fef4a75fbe54
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2025-01-18 03:10:12 +0200

    DAO first-Edition

--- AmazonProject/src/main/java/com/adminPanel/app/dao/ProductDAO.java
+++ AmazonProject/src/main/java/com/adminPanel/app/dao/ProductDAO.java
@@ -1,6 +1,16 @@
 package com.adminPanel.app.dao;
 
 
+import com.adminPanel.app.model.Product;
+import com.adminPanel.app.model.ProductDetails;
 
-public class ProductDAO {
+import java.util.List;
+
+public interface ProductDAO {
+
+     void addProduct(Product product);
+     void updateProduct(Product product);
+     void deleteProduct(int id);
+     Product findProductById(int id);
+     List<Product> getAllProducts();
 }
