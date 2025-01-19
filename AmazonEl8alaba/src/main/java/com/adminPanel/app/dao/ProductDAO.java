d7f5917139469bcdbcb62fc31c1835c2d44f42cf
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2025-01-19 19:52:48 +0200

    Service/Controller First-Edition

---
+++ AmazonEl8alaba/src/main/java/com/adminPanel/app/dao/ProductDAO.java
@@ -0,0 +1,15 @@
+package com.adminPanel.app.dao;
+
+
+import com.adminPanel.app.model.Product;
+
+import java.util.List;
+
+public interface ProductDAO {
+
+     void addProduct(Product product);
+     void updateProduct(Product product);
+     void deleteProduct(int id);
+     Product findProductById(int id);
+     List<Product> getAllProducts();
+}
---
+++ AmazonEl8alaba/src/main/java/com/adminPanel/app/service/ProductService.java
@@ -0,0 +1,39 @@
+package com.adminPanel.app.service;
+
+
+import com.adminPanel.app.dao.ProductDAO;
+import com.adminPanel.app.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Transient;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductDAO productDAO;
+    @Transactional
+    public void addProduct(Product product){
+        productDAO.addProduct(product);
+    }
+
+    @Transactional
+    public void updateProduct(Product product){
+        productDAO.updateProduct(product);
+    }
+
+    @Transactional
+    public Product findById(int id){
+        return productDAO.findProductById(id);
+    }
+
+    @Transactional
+    public List<Product> getAllProducts(){
+        return productDAO.getAllProducts();
+    }
+    @Transactional
+    public void deleteProduct(int id){productDAO.deleteProduct(id);}
+}
